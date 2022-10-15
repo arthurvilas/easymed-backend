4c8e994df864185190594ab3e043b61a1b8894f3
Arthur <arthurvilas5@gmail.com>
2022-10-14 21:16:24 -0300

    fix cors origins

--- src/database/knex/index.js
+++ src/database/knex/index.js
@@ -1,6 +1,6 @@
 const config = require('../../../knexfile');
 const knex = require('knex');
 
-const connection = knex(config.production);
+const connection = knex(config.development);
 
 module.exports = connection;
--- src/index.js
+++ src/index.js
@@ -7,7 +7,16 @@ const cookieParser = require('cookie-parser');
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+    origin: [
+      'http://localhost:5173/',
+      'http://127.0.0.1:5173/',
+      'https://easy-med-front.vercel.app/',
+    ],
+  })
+);
 app.use(express.json());
 app.use(cookieParser(process.env.JWT_SECRET));
 
